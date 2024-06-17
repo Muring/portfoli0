e95fd14aa35881cace6a6535daa83bca0af2567a
MuRing <11esh@naver.com>
2024-06-17 14:45:42 +0900

    Feat: Home card detail update

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,9 +1,17 @@
 import styles from "@/styles/card.module.scss";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function ProjectCard() {
   return (
     <div className={styles.card}>
-      <div></div>
+      <Link href={"/"}>
+        <Image src={"/"} width={100} height={100} alt="project image"></Image>
+        <div className={styles.description}>
+          <h4>Project Title</h4>
+          <p>Project description</p>
+        </div>
+      </Link>
     </div>
   );
 }
